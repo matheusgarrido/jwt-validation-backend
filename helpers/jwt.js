a32161e16f8368fd3f0d8e72f49b6ab7e626e1f9
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-08-15 19:14:56 -0300

    Secret Access Token renamed at .env file

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,13 +5,13 @@ require('dotenv/config');
 exports.signAccessToken = (userId) => {
   return new Promise((resolve, reject) => {
     const payload = {};
-    const { JWT_SECRET } = process.env;
+    const secret = process.env.ACCESS_TOKEN_SECRET;
     const options = {
       expiresIn: '1h',
       audience: userId,
       issuer: 'https://github.com/matheusgarrido',
     };
-    JWT.sign(payload, JWT_SECRET, options, (err, token) => {
+    JWT.sign(payload, secret, options, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
