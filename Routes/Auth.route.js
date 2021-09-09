6c853b1cde069ed11e90c9badc866de16dd036ca
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-09 13:13:44 -0300

    Refresh-token route HTTP method changed: POST to PATCH

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.post('/refresh-token', AuthController.refreshToken);
+router.patch('/refresh-token', AuthController.refreshToken);
 
 module.exports = router;
