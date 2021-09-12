46efd0b16659128330d68d05f4428043c8b1adce
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-12 13:41:10 -0300

    Data value validation at password, email, username and personal data changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const UserModel = require('../Models/User.model');
 const Database = require('../helpers/database');
+const Validation = require('../helpers/schemaValidation');
 const SchemaObject = require('../helpers/schemaObject');
 
 const getUserByIdRequested = async (req) => {
@@ -37,8 +38,16 @@ exports.changePassword = async (req, res, next) => {
   try {
     const user = req.user;
     const { newPassword } = req.body;
+    //If current password
     if (await user.isValidPassword(newPassword))
       throw createError.Conflict('Password already in use');
+    //Validate password
+    const passwordValidation = Validation.changePassword(newPassword);
+    if (passwordValidation.error) {
+      throw createError.BadRequest(passwordValidation.message);
+    }
+    //Save password
+    user.dt_last_update = passwordValidation.value.dt_last_update;
     await user.hashPassword(newPassword);
     user.save();
     res
@@ -53,13 +62,22 @@ exports.changeEmail = async (req, res, next) => {
   try {
     const user = req.user;
     const { email } = req.body;
+    //User's current email
     if (user.email === email)
       throw createError.Conflict('This is your current e-mail');
+    //Another user using this email
     const anotherUser = new UserModel(
       await Database.findOne('user', { email })
     );
     if (anotherUser.username)
       throw createError.Conflict('E-mail already in use by another user');
+    //Validate email
+    const emailValidation = Validation.changeEmail(email);
+    if (emailValidation.error) {
+      throw createError.BadRequest(emailValidation.message);
+    }
+    //Save email
+    user.dt_last_update = emailValidation.value.dt_last_update;
     user.email = email;
     user.save();
     res.status(200).json({ status: 200, message: 'Successful e-mail change' });
@@ -72,13 +90,22 @@ exports.changeUsername = async (req, res, next) => {
   try {
     const user = req.user;
     const { username } = req.body;
+    //User's current username
     if (user.username === username)
       throw createError.Conflict('This is your current username');
+    //Username used by another user
     const anotherUser = new UserModel(
       await Database.findOne('user', { username })
     );
     if (anotherUser.username)
       throw createError.Conflict('Username already in use by another user');
+    //Validate username
+    const usernameValidation = Validation.changeUsername(username);
+    if (usernameValidation.error) {
+      throw createError.BadRequest(usernameValidation.message);
+    }
+    //Save username
+    user.dt_last_update = usernameValidation.value.dt_last_update;
     user.username = username;
     user.save();
     res
@@ -93,6 +120,13 @@ exports.changePersonalData = async (req, res, next) => {
   try {
     const user = req.user;
     const { dt_birth, role } = req.body;
+    //Validate personal data
+    const dataValidation = Validation.changePersonalData({ dt_birth, role });
+    if (dataValidation.error) {
+      throw createError.BadRequest(dataValidation.message);
+    }
+    //Save data
+    user.dt_last_update = dataValidation.value.dt_last_update;
     user.dt_birth = dt_birth;
     user.role = role;
     user.save();
--- helpers/schemaValidation.js
+++ helpers/schemaValidation.js
@@ -2,9 +2,13 @@ const Joi = require('@hapi/joi');
 
 // Schema fields config value
 const EMAIL_FIELD = Joi.string().email().lowercase().required();
+const USERNAME_FIELD = Joi.string().min(3).required();
 const PASSWORD_FIELD = Joi.string().min(6).required();
-const CURRENT_DATE_FIELD = Joi.date().default(new Date());
+const DT_BIRTH_FIELD = Joi.date().required();
+const ROLE_FIELD = Joi.string().required();
+const ADMIN_FIELD = Joi.boolean();
 const REFRESH_TOKENS_FIELD = [Joi.string()];
+const CURRENT_DATE_FIELD = Joi.date().default(new Date());
 
 function setMessageError(validation) {
   if (validation.error) {
@@ -24,11 +28,11 @@ function validate(schema, data) {
 exports.userRegister = (user) => {
   const schema = Joi.object({
     email: EMAIL_FIELD,
-    username: Joi.string().min(3).required(),
+    username: USERNAME_FIELD,
     password: PASSWORD_FIELD,
-    dt_birth: Joi.date().required(),
-    role: Joi.string().required(),
-    admin: Joi.boolean(),
+    dt_birth: DT_BIRTH_FIELD,
+    role: ROLE_FIELD,
+    admin: ADMIN_FIELD,
     dt_creation: CURRENT_DATE_FIELD,
     dt_last_update: CURRENT_DATE_FIELD,
     refresh_token_list: REFRESH_TOKENS_FIELD,
@@ -42,4 +46,37 @@ exports.userLogin = (login) => {
     password: PASSWORD_FIELD,
   });
   return validate(schema, login);
+};
+
+exports.changePassword = (newPassword) => {
+  const schema = Joi.object({
+    password: PASSWORD_FIELD,
+    dt_last_update: CURRENT_DATE_FIELD,
+  });
+  return validate(schema, { password: newPassword });
+};
+
+exports.changeEmail = (email) => {
+  const schema = Joi.object({
+    email: EMAIL_FIELD,
+    dt_last_update: CURRENT_DATE_FIELD,
+  });
+  return validate(schema, { email });
+};
+
+exports.changeUsername = (username) => {
+  const schema = Joi.object({
+    username: USERNAME_FIELD,
+    dt_last_update: CURRENT_DATE_FIELD,
+  });
+  return validate(schema, { username });
+};
+
+exports.changePersonalData = (personalData) => {
+  const schema = Joi.object({
+    dt_birth: DT_BIRTH_FIELD,
+    role: ROLE_FIELD,
+    dt_last_update: CURRENT_DATE_FIELD,
+  });
+  return validate(schema, personalData);
 };
